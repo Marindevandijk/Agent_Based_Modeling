11fb11e40804145a15bdb62f272a269cf99ed9f9
matijsv <matijs.verloo@gmail.com>
2025-06-30 22:02:47 +0200

    added marinde's stuff

--- experiments.py
+++ experiments.py
@@ -48,7 +48,7 @@ def run_experiment(output_path, networked):
 
     model_out = model.datacollector.get_model_vars_dataframe()
     model_out.to_csv(output_path, index=False)
-    
+
 def plot_experiments(data_file_path, networked):
     """
     Plots epstein experiment results.
@@ -59,8 +59,6 @@ def plot_experiments(data_file_path, networked):
     networked : boolean
         whether the model is networked or not, will store figures in the appropriate Figures subdirectory
     """    
-    
-
     # Determine the Figures subdirectory based on 'networked'
     figures_dir = os.path.join('Figures', 'networked' if networked else 'non_networked')
     os.makedirs(figures_dir, exist_ok=True)
@@ -70,7 +68,7 @@ def plot_experiments(data_file_path, networked):
     active = df['active']
     time = range(len(active))
 
-    plt.plot(time[:1000], active[:1000])
+    plt.plot(time[:500], active[:500])
     plt.xlabel('Time', size=12)
     plt.ylabel('Number of active agents', size=12)
     plt.savefig(os.path.join(figures_dir, f'puncEq_net_{networked}.pdf'))
@@ -90,13 +88,51 @@ def plot_experiments(data_file_path, networked):
             last_end = i
             outburst = False
 
+
+    waiting_time = np.array(waiting_time)
+    waiting_time = waiting_time[waiting_time<280]
     min_wt, max_wt = min(waiting_time), max(waiting_time)
-    bins = np.arange(min_wt, max_wt + 2) - 0.5  # bin edges centered on integers
-    plt.hist(waiting_time, bins=bins)
+    bin_width = 3
+    bins = np.arange(min_wt, max_wt + bin_width, bin_width) - 0.5
+    print('mean',np.mean(waiting_time))
+    print('std',np.std(waiting_time))
+    cv = np.std(waiting_time) / np.mean(waiting_time)
+    print('CV', cv)
     
+    plt.hist(waiting_time, bins=bins,edgecolor="black")
     plt.xlabel("Waiting time between outbursts")
     plt.ylabel("Frequency")
     plt.savefig(os.path.join(figures_dir, f'Wait_Time_net_{networked}.pdf'))
+    plt.show()
+
+    # truncated waiting time from values above 30 
+    
+    waiting_time_trunc = waiting_time[waiting_time>30]
+    min_wt, max_wt = min(waiting_time_trunc), max(waiting_time_trunc)
+    bin_width = 3
+
+    counts, bin_edges = np.histogram(waiting_time_trunc, bins=bins)
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    nonzero = counts > 0
+    counts = counts[nonzero]
+    bin_centers = bin_centers[nonzero]
+
+    log_counts = np.log10(counts)
+    slope, intercept = np.polyfit(bin_centers, log_counts, 1)
+    fit_x = np.linspace(min(bin_centers), max(bin_centers), 100)
+    fit_y = 10 ** (slope * fit_x + intercept)
+    print(f"Slope of plot: {slope:.3f}")
+    
+    bins = np.arange(min_wt, max_wt + bin_width, bin_width) - 0.5
+    mid_x = 0.5 * (min(fit_x) + max(fit_x))
+    mid_y = 10 ** (slope * mid_x + intercept+ 0.5)
+
+    plt.hist(waiting_time_trunc, bins=bins,log=True,edgecolor="black")
+    plt.text(mid_x, mid_y, f"$y = {slope:.2f}x + {intercept:.2f}$", fontsize=10, color='red')
+    plt.plot(fit_x, fit_y, linestyle='dotted', color='red', label=f"Slope ≈ {slope:.2f}")
+    plt.xlabel("Waiting time between outbursts")
+    plt.ylabel("Frequency")
+    plt.savefig(os.path.join(figures_dir, f'Wait_Time_trunced_net_{networked}.pdf'))
     plt.show()
 
     # region EXPERIMENT 3 -------------------
@@ -109,6 +145,7 @@ def plot_experiments(data_file_path, networked):
     plt.legend()
     plt.savefig(os.path.join(figures_dir, f'tension_net_{networked}.pdf'))
     plt.show()
+
 
 if __name__ == "__main__":
     # uncomment to generate own data
